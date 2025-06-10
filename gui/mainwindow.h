de169477d730c531740afe7ce760474cb5db0724
Faisal Ali <124307904+faisalali2004@users.noreply.github.com>
2025-06-11 03:59:17 +0500

    Add files via upload
    mainwindow.cpp and mainwindow.h are improved to process the signal given out from rule_engine.cpp so that it can give pop up in interactive mode

--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -1,5 +1,16 @@
 #pragma once
+
 #include <QMainWindow>
+#include <QStackedWidget>
+#include <QToolBar>
+#include <QAction>
+#include "rule_engine.h" // For PacketInfo and RuleEngine
+
+class Dashboard;
+class LogViewer;
+class RuleEditor;
+class TrafficShaperUI;
+class DPIManager;
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -7,5 +18,35 @@ class MainWindow : public QMainWindow {
 public:
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
-    // ... other members ...
+
+private slots:
+    void showDashboard();
+    void showLogViewer();
+    void showRuleEditor();
+    void showTrafficShaper();
+    void showDPIManager();
+
+    // Interactive firewall popup slot
+    void onUserDecisionNeeded(const PacketInfo& pkt);
+
+private:
+    void setupNavigation();
+    void setupConnections();
+
+    QStackedWidget* stackedWidget;
+    Dashboard* dashboard;
+    LogViewer* logViewer;
+    RuleEditor* ruleEditor;
+    TrafficShaperUI* trafficShaperUI;
+    DPIManager* dpiManager;
+
+    QToolBar* navToolBar;
+    QAction* dashboardAction;
+    QAction* logAction;
+    QAction* ruleAction;
+    QAction* shaperAction;
+    QAction* dpiAction;
+
+    // Pointer to your RuleEngine instance
+    RuleEngine* ruleEngine;
 };
